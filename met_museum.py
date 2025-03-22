0f039771e3fcedbb221ae92074be02d786d779ce
kamillamamatova <kamillamamatova.km@gmail.com>
2025-03-22 16:43:18 -0400

    Update met_museum.py

--- met_museum.py
+++ met_museum.py
@@ -2,3 +2,4 @@ artifact_info = {'artist': 'Frederic Edwin Church', 'period': 'idk', 'date': '18
 
 print("This is the dictionary of the artifact:", artifact_info)
 print("This is the keys of the dictionary of the artifact:", artifact_info.keys())
+print("This is the values of the dictionary:", artifact_info.values())
